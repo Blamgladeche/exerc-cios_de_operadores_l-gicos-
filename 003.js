efc5f48d8501b864918cd241ce561107e0370d06
Blamgladeche <116769323+Blamgladeche@users.noreply.github.com>
2023-03-28 00:17:29 -0300

    exercícios

---
+++ 003.js
@@ -0,0 +1,68 @@
+ {
+ let a = 6
+ let b = 7
+ if (a > 5 && b > 5) {
+  console.log('True')
+} else {
+  console.log('False')
+ }
+}
+{
+ let a = 1
+ if (a > 0) {
+  console.log('True')
+ } else {
+  console.log('False')
+ }
+}
+{
+ let a = 3
+ if (a * 5 == 15 && a * 3 == 9) {
+  console.log('True')
+ } else {
+  console.log('False')
+ }
+}
+{
+ let a = 5
+ let b = 6
+ if (a > 10 || b == 6) {
+  console.log('True')
+ } else {
+  console.log('False')
+ }
+}
+{
+ let a = 5
+ let b = 6
+ let c = 7
+ if (a == 5 || b == 6 && c == 8) {
+  console.log('True')
+ } else {
+  console.log('False')
+ }
+}
+{
+ let a = 'Caminhão'
+ if (a.length > 5 && a.length < 10) {
+  console.log('True')
+ } else {
+  console.log('False')
+ }
+}
+{
+ let a = 15
+ if (a > 10 && a < 20) {
+  console.log('True')
+ } else {
+  console.log('False')
+ }
+}
+{
+ let a = 4
+ if (a < 5 && a < 15) {
+  console.log('True')
+ } else {
+  console.log('False')
+ }
+}
